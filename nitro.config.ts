7a85a688b789d36d58e256fda147b369c161cd77
JCadiente <cadientejomel0@gmail.com>
2026-03-15 13:39:54 +0800

    chore: initial config

---
+++ nitro.config.ts
@@ -0,0 +1,12 @@
+import { defineNitroConfig } from "nitro/config";
+
+export default defineNitroConfig({
+  compatibilityDate: "2024-09-19",
+  ...(process.env.NODE_ENV === 'production' && {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+  }),
+})
