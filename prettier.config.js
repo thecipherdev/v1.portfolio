a48c3a1e159a9fee94b174dfc20c374e9777ebcb
thecipherdev <160809908+thecipherdev@users.noreply.github.com>
2026-03-15 14:07:22 +0800

    Add initial config (#1)
    * chore: initial config
    
    * fix: cloduflare build error
    
    * fix: cloduflare build error
    
    ---------
    
    Co-authored-by: JCadiente <cadientejomel0@gmail.com>

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,24 @@
 
 /** @type {import('prettier').Config} */
 const config = {
-  semi: false,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   trailingComma: "all",
+  importOrder: [
+    "^(react$)|^(react/(.*)$)",
+    "^(@tanstack$)|^(@tanstack/(.*)$)",
+    "<THIRD_PARTY_MODULES>",
+    "^@sanity/(.*)$",
+    "^@hooks/(.*)$",
+    "^@lib/(.*)$",
+    "^@ui/(.*)$|^@components/(.*)$",
+    "^@(.*)$",
+    "^([./])|^([../])"
+  ]
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,29 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
+import { sentryTanstackStart } from "@sentry/tanstackstart-react/vite";
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
+// import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    // nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    sentryTanstackStart({
+      org: "acme-99",
+      project: "javascript-tanstackstart-react",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
   ],
 })
 
